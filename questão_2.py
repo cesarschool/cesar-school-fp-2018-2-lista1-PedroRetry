8efdd82ee489fba9f1ad6ff69192620702c83360
Pedro Barbosa <42580441+PedroRetry@users.noreply.github.com>
2018-09-05 15:39:32 -0300

    questão_2

---
+++ questão_2.py
@@ -0,0 +1,6 @@
+def main():
+    p = float(input('quantos graus está nesse momento?')) #p=pergunta e f=fórmula
+    f = 1.8*p+32
+    print('lá fora está', p, 'graus em celsius', 'e', f, 'graus em fahrenheit')
+if __name__ == '__main__':
+    main()
