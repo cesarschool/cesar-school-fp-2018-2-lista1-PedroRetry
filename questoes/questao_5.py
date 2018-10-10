ee1da24c3e7181e820ff256da7645f323f33ae10
Pedro Barbosa <42580441+PedroRetry@users.noreply.github.com>
2018-10-10 10:56:31 -0300

    Update questao_5.py

--- questoes/questao_5.py
+++ questoes/questao_5.py
@@ -13,9 +13,11 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 5")
-
-
-    
+    c = int(input('quantos cigarros você fuma por dia'))
+    a = int(input('por quantos anos você vem fumando')) #c=cigarros a=anos a1=1 ano tem 365 dias,quantidade de anos em dias 
+    a1 = a*365
+    c1 = c*a1
+    d = c1/144
+    print('você perderá', d, 'dias de vida')
 if __name__ == '__main__':
     main()
