4dd7577b3065f5dae391458e831d3d92c7c0f852
Pedro Barbosa <42580441+PedroRetry@users.noreply.github.com>
2018-10-10 10:45:08 -0300

    Update questao_2.py

--- questoes/questao_2.py
+++ questoes/questao_2.py
@@ -11,9 +11,8 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 2")
-
-
-
+   p = float(input('quantos graus está nesse momento?')) #p=pergunta e f=fórmula
+   f = 1.8*p+32
+   print('lá fora está', p, 'graus em celsius', 'e', f, 'graus em fahrenheit')
 if __name__ == '__main__':
     main()
