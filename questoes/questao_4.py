6fe0d63ab56945ce571893f37437ea6cee4f3bb2
Pedro Barbosa <42580441+PedroRetry@users.noreply.github.com>
2018-10-10 10:55:18 -0300

    Update questao_4.py

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -11,9 +11,11 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 4")
-
-
-    
+    d = int(input('por quantos dias você rodou com o carro?')) #d=número de dias
+    k = float(input('quantos quilômetros você rodou nesses dias?')) #K=quilômetros rodados o=orçamento
+    o1 = 60*d
+    o2 = 0.15*k
+    of = o1+o2
+    print('você terá que pagar R$', of)
 if __name__ == '__main__':
     main()
