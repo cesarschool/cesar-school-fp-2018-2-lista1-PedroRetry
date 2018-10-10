32fdb897ae800fbcd1585e6c899c90557398e855
Pedro Barbosa <42580441+PedroRetry@users.noreply.github.com>
2018-10-10 10:43:56 -0300

    Update questao_1.py

--- questoes/questao_1.py
+++ questoes/questao_1.py
@@ -11,9 +11,10 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-    print("questao 1")
-    
-
-
+    S = int(input('quanto você ganha?')) #S=Salário
+    A = int(input('em quantos porcento seu salário aumentou: ')) #A=Aumento
+    P = S*A/100 #P=porcentagem
+    R = S+P #R=resultado
+    print('seu aumento foi de R$', P, 'e seu novo salário é de R$', R)
 if __name__ == '__main__':
-    main()
+     main()
